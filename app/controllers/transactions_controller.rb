7038c74bf61ef227cefd2eebcff000be3ffb72fc
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-05 14:48:21 -0400

    added display action and view to transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,13 +1,17 @@
 class TransactionsController < ApplicationController
   def display
+    @items = Item.all
   end
 
   def notice
+
   end
 
   def check_in
+    @item = Item.find(params[:id])
   end
 
   def check_out
+
   end
 end
