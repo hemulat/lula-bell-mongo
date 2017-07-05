0ce89340044436420950da5378bf94ee7bf29316
Roy Bhattacharya <anroybhattacharya@w14581w.davidson.edu>
2017-07-05 10:54:00 -0400

    Commit before devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :blogs
   get '/items/new', to: 'items#select', as: :new_item
   post '/items/new', to: 'items#select'
   get '/items/new/:class', to: 'items#new'
 
   resources :items,  except:[:new]
+
+  root to: 'home#index'
 
 end
