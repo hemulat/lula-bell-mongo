1a956696de492f805b629562cef5512193e4e756
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-05 11:13:39 -0400

    updated transaction modle

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,7 @@
 class Transaction
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Attributes::Dynamic
   field :student_id, type: Integer
   field :start_date, type: Date
   field :end_date, type: Date
