bb87c8e6a5b19475e739b9f5d37f2645f9560c44
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-05 15:59:30 -0400

    added create action for display view; created show templatefor display view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -9,9 +9,28 @@ class TransactionsController < ApplicationController
 
   def check_in
     @item = Item.find(params[:id])
+    @transaction = Transaction.new()
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    #Save the object
+    if @item.save
+      #If save succeeds, redirect to the index action
+      flash[:notice] = "Transaction created successfully."
+      redirect_to(:action => 'display')
+    else
+      #If save fails, redisplay the form so user can fix problems
+      render('new') # this renews the form template
+    end
   end
 
   def check_out
 
+  end
+
+  private
+  def transaction_params
+    params.require(:transaction).permit(:student_id, :start_date, :end_date, :status)
   end
 end
