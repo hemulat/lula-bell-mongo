277413313d5ef94ced26b337b57eb300b5c1c0fa
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-05 18:05:25 -0400

    added check in functions;

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,23 +4,37 @@ class TransactionsController < ApplicationController
   end
 
   def notice
-    @transactions = Transaction.all()
+    @transactions = Transaction.all
   end
 
   def check_in
-
+    @transaction = Transaction.find(params[:id])
   end
 
   def create
     @transaction = Transaction.new(transaction_params)
     #Save the object
-    if @item.save
+    if @transaction.save
       #If save succeeds, redirect to the index action
       flash[:notice] = "Transaction created successfully."
       redirect_to(:action => 'display')
     else
       #If save fails, redisplay the form so user can fix problems
       render('new') # this renews the form template
+    end
+  end
+
+  def update
+    #Find a new object using form parameters
+    @transaction = Transaction.find(params[:id])
+    #Update the object
+    if @transaction.update_attributes(transaction_params)
+      #If save succeeds, redirect to the show action
+      flash[:notice] = "Transaction updated successfully."
+      redirect_to(:action => 'notice')
+    else
+      #If save fails, redisplay the form so user can fix problems
+      render('edit') # this renews the form template
     end
   end
 
