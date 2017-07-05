9434533bfed04e1b776b6300d315e8e3a47306dc
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-05 13:40:43 -0400

    added transaction controller and views to project

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,13 @@
+class TransactionsController < ApplicationController
+  def display
+  end
+
+  def notice
+  end
+
+  def check_in
+  end
+
+  def check_out
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'transactions/display'
+
+  get 'transactions/notice'
+
+  get 'transactions/check_in'
+
+  get 'transactions/check_out'
+
   resources :blogs
   get '/items/new', to: 'items#select', as: :new_item
   post '/items/new', to: 'items#select'
