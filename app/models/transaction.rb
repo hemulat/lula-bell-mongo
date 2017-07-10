5e88cc3d37aa99b387643eadc9f90810c28e53ee
davidson2017git <ahidalg2@email.cpcc.edu>
2017-07-10 14:29:18 -0400

    merged test to andre-ti

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -9,8 +9,6 @@ class Transaction
   field :end_date, type: Date
   field :status, type: String
 
-  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-
   validates_presence_of :student_id
   validates_confirmation_of :student_id
   validates :email, :presence => true
