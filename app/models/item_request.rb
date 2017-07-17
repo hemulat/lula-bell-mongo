ed8a14a58b17f7837965bc93ff41d9aa0105a5d5
Hermon <hemulat@davidson.edu>
2017-07-17 09:53:05 -0400

    Add form validation for item_request

--- app/models/item_request.rb
+++ app/models/item_request.rb
@@ -4,4 +4,11 @@ class ItemRequest
   field :title, type: String
   field :description, type: String
   field :studentID, type: Integer
+
+  validates_presence_of :studentID
+  validates_presence_of :description
+  validates_presence_of :title
+
+  validates_length_of :description, minimum: 50
+  validates_length_of :studentID, minimum:9, maximum:9, message: "must be exactly 9 digits"
 end
