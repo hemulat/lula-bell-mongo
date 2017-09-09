735833a433d1155ea1cbbb127b687d72aa23c4fb
root <root@MCS3.davidson.edu>
2017-09-09 16:04:20 -0400

    Fix a bug in load_books script

--- app/scripts/load_books.rb
+++ app/scripts/load_books.rb
@@ -1,6 +1,6 @@
 require 'csv'
 def read_csv(filename = 'books.csv')
-    keys = ['title','author', 'ISBN', 'description', 'quantity','type']
+    keys = ['name','author', 'ISBN', 'description', 'quantity','type']
     data = CSV.read(filename)
     string_data = data.map {|row| row.map {|cell| cell.to_s } }
     books = string_data.map {|row| Hash[*keys.zip(row).flatten] }
@@ -8,7 +8,6 @@ def read_csv(filename = 'books.csv')
       b['rentable'] = true
       b['reservable'] = false
       b['_SKU'] = Book.next_sku
-      book = Book.new(b)
-      book.save
+      Book.create(b)
     end
 end
